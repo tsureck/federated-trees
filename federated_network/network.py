c1cfbb00bc01c4d83150fb43df852b063dfe3afa
tsureck <tom.sureck@t-online.de>
2026-01-25 23:31:08 +0100

    Fix is_drift property for drifts that stay applied

--- federated_network/network.py
+++ federated_network/network.py
@@ -116,7 +116,10 @@ class FederatedNetwork:
         for _round in range(self.num_training_rounds):
             # Add drift to the clients, if within the drift period
             self.drift.current_round = _round
-            if self.drift.drift_start_round <= _round <= self.drift.drift_end_round:
+            if self.drift.drift_pattern == constants.DriftPatterns.GRADUAL_REOCCURRING:
+                if self.drift.drift_start_round <= _round <= self.drift.drift_end_round:
+                    self.drift.is_drift = True
+            elif self.drift.drift_start_round <= _round:
                 self.drift.is_drift = True
 
                 # Modify the client groups if the drift is asynchronous
